a828fdc45cddbc3605dfbf913b0ad3f155e29cdb
Jorge Artave <jorge@xmartlabs.com>
2019-12-01 18:21:27 -0300

    Initial version

---
+++ learning_logs/models.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Topic(models.Model):
+    """ A topic the user is talking bout"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    def __unicode__(self):
+        return self.text
+
+class Entry(models.Model):
+    """ A lesson learned about a topic """
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __unicode__(self):
+        if self.text is None:
+            return ''
+        
+        if len(self.text) > 50:
+            return self.text[:50] + '...'
+
+        return self.text
+            
