e279ac001c0c6d5cab3008f16f14478f0195b149
Jorge Artave <jorge@xmartlabs.com>
2019-12-01 22:22:04 -0300

    Add homepage, topics page and topic detail page

---
+++ learning_logs/urls.py
@@ -0,0 +1,15 @@
+"""URL patterns for learning_logs"""
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # Home page
+    url(r'^$', views.index, name='index'),
+
+    # Show all topics
+    url(r'^topics/$', views.topics, name='topics'),
+    # Topic detail page
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+from learning_logs.models import Topic
+
+def index(request):
+    """Home page for Learning Logs"""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Show all topics"""
+    context = {'topics': Topic.objects.all()}
+
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    
+    return render(request, 'learning_logs/topic.html', context)
